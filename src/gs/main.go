2ae0dfb55d572da7e83956c539f4882f91a66113
Rebel-L <dj@rebel-l.net>
2016-03-20 18:04:28 +0100

    implemented go example

--- src/gs/main.go
+++ src/gs/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"encoding/json"
+)
+
+type Data struct {
+	Sentence	string
+}
+
+func handler(response http.ResponseWriter, request *http.Request) {
+	name := "World";
+	if (request.URL.Query().Get("name") != "") {
+		name = request.URL.Query().Get("name");
+	}
+
+	myData := Data{
+		Sentence: "Hello " + name + "!",
+	}
+	response.Header().Set("Content-Type", "application/json;");
+	json.NewEncoder(response).Encode(myData);
+}
 
 func main() {
-	fmt.Println("Hello World");
+	http.HandleFunc("/", handler);
+	http.ListenAndServe(":8080", nil);
 }
